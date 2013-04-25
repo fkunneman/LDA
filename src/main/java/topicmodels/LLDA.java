b6364148821100fb3d532d42eacc3a04e0b510f7
Folgert Karsdorp <fbkarsdorp@gmail.com>
2013-04-25 18:19:33 +0200

    more documentation before dinner

--- src/main/java/topicmodels/LLDA.java
+++ src/main/java/topicmodels/LLDA.java
@@ -42,6 +42,13 @@ public class LLDA implements Serializable {
     protected Randoms random;
     protected Boolean trained = false;
 
+    /**
+     * Initialize an instance of LLDA.
+     *
+     * @param alpha smoothing parameter over the topic distribution;
+     * @param beta smoothing parameter over the unigram distribution;
+     * @param corpus the corpus from which to learn the distributions;
+     */
     public LLDA (double alpha, double beta, Corpus corpus) {
         numTopics = corpus.getNumTopics();
         numWords = corpus.getNumWords();
@@ -58,6 +65,13 @@ public class LLDA implements Serializable {
         random = new Randoms(20);
     }
 
+    /**
+     * Given a corpus, where each document has been assigned to a set of topics,
+     * learn the topic distributions per document, and the word distributions of topics.
+     *
+     * @param iterations how many iterations to run the sampler;
+     * @param corpus the corpus to run the sampler on;
+     */
     public void train (int iterations, Corpus corpus) {
         learnSampler = new LLDA.LearnSampler();
         for (Document document : corpus) {
@@ -73,6 +87,13 @@ public class LLDA implements Serializable {
         trained = true;
     }
 
+    /**
+     * Given a corpus of test documents, try to assign to each token
+     * a topic based on a previously learned LLDA model.
+     *
+     * @param iterations how many iterations to run the sampler;
+     * @param corpus the corpus to run the sampler on;
+     */
     public void infer (int iterations, Corpus corpus) {
         inferSampler = new LLDA.InferSampler();
         for (Document document : corpus) {
@@ -87,6 +108,14 @@ public class LLDA implements Serializable {
         }
     }
 
+    /**
+     * Write the either learned or inferred topic distributions to a file.
+     *
+     * @param file the name of the file to write the results;
+     * @param corpus the corpus containing the topic and type assignments;
+     * @param smooth parameter to use for smoothing the topic distributions on output;
+     * @throws IOException
+     */
     public void writeTopicDistributions (File file, Corpus corpus, double smooth) throws IOException {
         PrintWriter printer = new PrintWriter(file);
         printer.print("source\ttopic:proportion...\n");
@@ -115,8 +144,17 @@ public class LLDA implements Serializable {
         printer.close();
     }
 
+    /**
+     * Base sampler, sub-classed by LearnSampler and InferSampler.
+     */
     public class Sampler {
 
+        /**
+         * Sample the topics for all tokens of a document.
+         *
+         * @param document an instance of Document for which we sample the topics;
+         * @param labels the set of possible labels to sample from for this document;
+         */
         public void sampleForOneDocument (Document document, ArrayList<Integer> labels) {
             int[] docTopicCounts = new int[numTopics];
             for (Integer topic : document.getTopicAssignments()) {
@@ -137,10 +175,23 @@ public class LLDA implements Serializable {
             }
         }
 
+        /**
+         * Sample the topics for all tokens of a document.
+         *
+         * @param document an instance of Document for which we sample the topics;
+         */
         public void sampleForOneDocument (Document document) {
             sampleForOneDocument(document, document.getLabels());
         }
 
+        /**
+         * Add a document to the sampler, which means that we randomly assign to each token
+         * a topic (sampled from the set of topics in labels).
+         * Increment the new assigned topic in the count matrices.
+         *
+         * @param document an instance of Document for which to do the random assignments;
+         * @param labels the set of possible labels to sample from;
+         */
         public void addDocument (Document document, ArrayList<Integer> labels) {
             for (int position = 0; position < document.size(); position++) {
                 int topic = random.choice(labels);
@@ -152,6 +203,15 @@ public class LLDA implements Serializable {
         public void increment (int topic, int word) {}
         public void decrement (int topic, int word) {}
 
+        /**
+         * Sample a topic for the current word. This method is computationally
+         * quite heavy and should and could probably be optimized further.
+         *
+         * @param word the word for which we sample a topic and a type;
+         * @param labels the set of labels to sample a topic from;
+         * @param docTopicCounts for each topic, how often does it occur in the document under investigation?
+         * @return the sampled topic
+         */
         public int sample (int word, ArrayList<Integer> labels, int[] docTopicCounts) {
             double[] topicTermScores = new double[labels.size()];
             double sum = 0.0;
@@ -174,21 +234,42 @@ public class LLDA implements Serializable {
 
     public class LearnSampler extends LLDA.Sampler {
 
+        /**
+         * Add a document to the sampler, which means that we randomly assign to each
+         * token a topic (sampled from the set of topic associated with this document).
+         *
+         * @param document an instance of Document for which to do the random assignments;
+         */
         public void addDocument (Document document) {
             addDocument(document, document.getLabels());
         }
 
+        /**
+         * Update the count matrices by incrementing the appropriate counts.
+         *
+         * @param topic the topic to update;
+         * @param word the word to update;
+         */
         public void increment (int topic, int word) {
             topicCounts[topic]++;
             wordTopicCounts[word][topic]++;
         }
 
+        /**
+         * Update the count matrices by decrementing the appropriate counts.
+         *
+         * @param topic the topic to update;
+         * @param word the word to update;
+         */
         public void decrement (int topic, int word) {
             topicCounts[topic]--;
             wordTopicCounts[word][topic]--;
         }
     }
 
+    /**
+     * Sampler for inference on unseen documents.
+     */
     public class InferSampler extends LLDA.Sampler {
 
         private ArrayList<Integer> documentTopics;
@@ -200,15 +281,34 @@ public class LLDA implements Serializable {
             }
         }
 
+        /**
+         * Add a document to the sampler, which means that we randomly assign to each
+         * token a topic sampled from all possible topics learned during training.
+         *
+         * @param document an instance of Document for which to do the random assignments;
+         */
         public void addDocument (Document document) {
             addDocument(document, documentTopics);
         }
 
+        /**
+         * Sample the topics for all tokens in the document.
+         *
+         * @param document an instance of Document for which we sample the topics;
+         */
         public void sampleForOneDocument (Document document) {
             sampleForOneDocument(document, documentTopics);
         }
     }
 
+    /**
+     * Read an existing serialized model from disk.
+     *
+     * @param file the filename of the model to read;
+     * @return the model;
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
     public static LLDA read (File file) throws IOException, ClassNotFoundException {
         LLDA llda;
         ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(file));
@@ -236,6 +336,12 @@ public class LLDA implements Serializable {
         trained = inputStream.readBoolean();
     }
 
+    /**
+     * Write a model serialized to disk.
+     *
+     * @param file the name of the file to write the model to;
+     * @throws IOException
+     */
     public void write (File file) throws IOException {
         ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(file));
         outputStream.writeObject(this);
