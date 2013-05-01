d87582bf78729328a08bf6a57995f8d8183e4727
Folgert Karsdorp <fbkarsdorp@gmail.com>
2013-05-01 11:34:19 +0200

    small updates

--- src/main/java/topicmodels/ProtoLDA.java
+++ src/main/java/topicmodels/ProtoLDA.java
@@ -1,10 +1,10 @@
 package topicmodels;
 
-import util.Corpus;
-import util.Document;
-import util.Index;
-import util.Randoms;
+import util.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.*;
 import java.util.logging.Logger;
 
@@ -16,6 +16,7 @@ public class ProtoLDA {
     InferSampler inferSampler;
 
     protected int regularTopics;
+    protected int numProtoTopics;
     protected int totalTopics;
     protected int numWords;
 
@@ -36,13 +37,13 @@ public class ProtoLDA {
 
     public ProtoLDA(int numTopics, double alpha, double beta, double gamma,
                     Corpus corpus, HashMap<String, ArrayList<String>> protoTopics) {
-        this.regularTopics = numTopics;
+        regularTopics = numTopics;
         this.alpha = alpha;
         this.beta = beta;
         this.gamma = gamma;
 
         topicIndex = new Index();
-        for (int topic = 0; topic < numTopics; topic++) {
+        for (int topic = 0; topic < regularTopics; topic++) {
             topicIndex.put("Topic: " + topic);
         }
 
@@ -75,8 +76,9 @@ public class ProtoLDA {
             topic++;
         }
 
-        this.totalTopics = topicIndex.size();
-        this.numWords = wordIndex.size();
+        totalTopics = topicIndex.size();
+        numProtoTopics = protoTopics.size();
+        numWords = wordIndex.size();
         // define the new betaSum by incorporating the prototypical words and their gamma value.
         betaSum = corpus.getNumTypes() * beta + gamma * protoWords.size();
         random = new Randoms(20);
@@ -131,9 +133,52 @@ public class ProtoLDA {
         }
     }
 
+    /**
+     * Write the either learned or inferred topic distributions to a file.
+     *
+     * @param file the name of the file to write the results;
+     * @param corpus the corpus containing the topic and type assignments;
+     * @param smooth parameter to use for smoothing the topic distributions on output;
+     * @throws java.io.IOException
+     */
+    public void writeTopicDistributions (File file, Corpus corpus, double smooth) throws IOException {
+        PrintWriter printer = new PrintWriter(file);
+        printer.print("source\ttopic:proportion...\n");
+        for (Document document : corpus) {
+            printer.print(document.getSource() + "\t");
+            IDSorter[] sortedTopics = new IDSorter[totalTopics];
+            int[] topicCounts = new int[totalTopics];
+            int docLen = 0;
+            for (int position = 0; position < document.size(); position++) {
+                int word = document.getToken(position);
+                if (word >= numWords) { continue; }
+                docLen++;
+                topicCounts[document.getTopic(position)]++;
+            }
+            for (int topic = 0; topic < totalTopics; topic++) {
+                sortedTopics[topic] = new IDSorter(topic, (smooth + topicCounts[topic]) / (docLen));
+            }
+            Arrays.sort(sortedTopics);
+            for (int index = 0; index < totalTopics; index++) {
+                double score = sortedTopics[index].getValue();
+                if (score == 0.0) { break; }
+                printer.print(corpus.getLabelIndex().getItem(sortedTopics[index].getIndex()) + " " + score + " ");
+            }
+            printer.print("\n");
+        }
+        printer.close();
+    }
 
+    /**
+     * Base sampler, sub-classed by LearnSampler and InferSampler
+     */
     public class Sampler {
 
+        /**
+         * Sample the topics for all tokens of a document.
+         *
+         * @param document an instance of Document for which we sample the topics;
+         */
         public void sampleForOneDocument (Document document) {
             int[] docTopicCounts = new int[totalTopics];
             for (Integer topic : document.getTopicAssignments()) {
@@ -157,6 +202,12 @@ public class ProtoLDA {
         public void increment (int topic, int word) {}
         public void decrement (int topic, int word) {}
 
+        /**
+         * Add a document to the sampler, which means that we randomly assign to
+         * each token a topic.
+         *
+         * @param document an instance of Document for which to do the random assignments;
+         */
         public void addDocument (Document document) {
             for (int position = 0; position < document.size(); position++) {
                 int topic = random.nextInt(totalTopics);
@@ -165,20 +216,29 @@ public class ProtoLDA {
             }
         }
 
+        /**
+         * Sample a topic for the current word. This method in particular is computationally
+         * expensive and could probably be optimized (further).
+         *
+         * @param word the word for which we sample a topic;
+         * @param docTopicCounts for each topic, how often does it occur in the document under investigation?
+         * @return the newly sampled topic.
+         */
         public int sample(int word, int[] docTopicCounts) {
             double[] termTopicScores = new double[totalTopics];
             double sum = 0.0;
-            for (int topic = 0; topic < totalTopics; topic++) {
-                double score;
-                if (topic < regularTopics) {
-                    score = (alpha + docTopicCounts[topic]) *
-                            (beta + wordTopicCounts[word][topic]) / (betaSum + topicCounts[topic]);
-                } else {
-                    score = (alpha + docTopicCounts[topic]) *
-                            (protoBeta[(topic-regularTopics)][word] + wordTopicCounts[word][topic]) / (betaSum + topicCounts[topic]);
-                }
+            for (int topic = 0; topic < regularTopics; topic++) {
+                double score = (alpha + docTopicCounts[topic]) *
+                        (beta + wordTopicCounts[word][topic]) / (betaSum + topicCounts[topic]);
                 sum += score;
                 termTopicScores[topic] = score;
+            }
+            for (int topic = 0; topic < numProtoTopics; topic++) {
+                int realTopic = regularTopics + topic;
+                double score = (alpha + docTopicCounts[realTopic]) *
+                        (protoBeta[topic][word] / (betaSum + topicCounts[realTopic]));
+                sum += score;
+                termTopicScores[realTopic] = score;
             }
             double sample = random.nextUniform() * sum;
             int topic = -1;
@@ -193,19 +253,37 @@ public class ProtoLDA {
         }
     }
 
+    /**
+     * Sampler for training a model
+     */
     public class LearnSampler extends Sampler {
 
+        /**
+         * Update the count matrices by incrementing the appropriate counts;
+         *
+         * @param topic the topic to update;
+         * @param word the word to update;
+         */
         public void increment (int topic, int word) {
             topicCounts[topic]++;
             wordTopicCounts[word][topic]++;
         }
 
+        /**
+         * Update the count matrices by decrementing the appropriate counts;
+         *
+         * @param topic the topic to update;
+         * @param word the word to update;
+         */
         public void decrement (int topic, int word) {
             topicCounts[topic]--;
             wordTopicCounts[word][topic]--;
         }
     }
 
+    /**
+     * Sampler for inference on unseen documents.
+     */
     public class InferSampler extends Sampler {}
 
 }
